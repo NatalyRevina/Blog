d18958128ad84f7f27dbec2b9577dbd21d4921f3
StudentTC <StudentTC@belhard.com>
2017-12-18 21:24:12 +0300

    jquery,  плагин

---
+++ jss1/about.js
@@ -0,0 +1,25 @@
+$(document).ready(function){
+
+
+$('.trip').click(function()){
+
+$('.locker, #modal').show();
+	var childHtml = $(this).html();
+	var child = $
+$('#modal').append($('<div/>')
+			.addClass('trip')
+			.append(childHtml);
+	
+			$('#modal').append(child);
+)};
+
+$('#modal > a').click(function(){
+	
+$('.locker, #modal').hide();
+$('#modal')
+
+.children('div')
+.remove();
+
+});
+});
---
+++ jss1/jquery.modal.js
@@ -0,0 +1,29 @@
+(function($){
+var defaults = {
+	locker: '.locker',
+	modal: '#modal'
+};
+$.fn.modal = function(settings){
+	var currentSettings = Object.assign({})
+
+$(locker + ','+ modal).show();
+	var childHtml = $(this).html();
+	var child = $('<'+ this.tagName+'/>')
+$(modal).append($('<div/>')
+			.addClass('trip')
+			.append(childHtml);
+	
+			$(modal).append(child);
+	$(modal + '>a').click(function(){
+)};
+
+	
+$('.locker, #modal').hide();
+$('#modal')
+
+.children('div')
+.remove();
+
+});
+});
+}(jQuery)
